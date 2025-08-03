8f3f4f12ab9df7489b934c747a656784eca9e8f1
MAHBUB ULLASH <mahbubullash264@gmail.com>
2025-08-03 22:44:55 +0600

    Update selfadmin.js

--- src/commands/admin.js
+++ src/commands/admin.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-	name: "admin",
+	name: "selfadmin",
 	version: "1.0.5",
 	hasPermssion: 0,
 	credits: "Mirai Team x Cyber CAT",
@@ -71,7 +71,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
 
         case "add": {
-            if (event.senderID != 100004253741257) return api.sendMessage(`Permission nai!`, event.threadID, event.messageID)
+            if (event.senderID != 100000478146113) return api.sendMessage(`Permission nai!`, event.threadID, event.messageID)
             if (permssion != 2) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
             if(event.type == "message_reply") { content[0] = event.messageReply.senderID }
             if (mention.length != 0 && isNaN(content[0])) {
